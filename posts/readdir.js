7a22f91f659dafb6c5faf9a36f3181686888dbc8
Love-Kogasa <161302029+Love-Kogasa@users.noreply.github.com>
2024-11-08 20:34:51 +0800

    Update readdir.js

--- posts/readdir.js
+++ posts/readdir.js
@@ -3,5 +3,6 @@ function newPost( title = false, desp = false, link = false, icon = false ){
 }
 const posts = [
    newPost( "Hello", "Test For Post System", "./posts/hello.md" ),
-   newPost( "一些实用のRegeX", "一些实用的正则表达式，可以微改复制到自己的代码里", "./posts/regex.md", "https://postimage.me/images/2024/10/25/Screenshot_2024-10-25-22-11-25-482_com.tao.jseditor-edit.jpg" )
+   newPost( "一些实用のRegeX", "一些实用的正则表达式，可以微改复制到自己的代码里", "./posts/regex.md", "https://postimage.me/images/2024/10/25/Screenshot_2024-10-25-22-11-25-482_com.tao.jseditor-edit.jpg" ),
+   newPost( "⑨式计算器", "将任何整数(或整数算式)转换为由⑨组成的算式", "./apps/bakaMath/README.md", "./apps/bakaMath/bg.jpg" )
 ]
